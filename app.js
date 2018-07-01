465e7dc7e5dfd3346e1b33c46725e1b358b9fd6a
VladiG1984 <vladig1984@gmail.com>
2018-07-02 00:36:41 +0300

    Created subscribeModal and logInModal

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-//$('.carousel').carousel(data-pause'hover');
+// Add document ready function to prevent incorrect performance on website (i.e., first load website, then perform js/ jQuery code)
+//$(document).ready(function () {
+    $('#logInForm').hide();
+
+    $('#logIn').click(function(){
+        $('#logInForm').show();
+    });
+//});
